436b042f7d3b79c0b96c49c27252573ee7a17c66
Nosferya <clement.polito@gmail.com>
2018-05-17 11:37:27 +0200

    tt

--- afficher_cacher_div.js
+++ afficher_cacher_div.js
@@ -1,14 +1,32 @@
 function afficher_cacher(id)
 {
+  var texte = document.getElementById(id)
+  var bouton = document.getElementById('bouton')
+    if(texte.style.visibility=="hidden")
+    {
+        texte.style.visibility="visible";
+        bouton.innerHTML='Coordonnées';
+    }
+    else
+    {
+        texte.style.visibility="hidden";
+        bouton.innerHTML='Coordonnées';
+    }
+    return true;
+}
+
+/* function afficher_cacher(id)
+{
     if(document.getElementById(id).style.visibility=="hidden")
     {
         document.getElementById(id).style.visibility="visible";
-        document.getElementById('bouton_'+id).innerHTML='Coordonnées';
+        document.getElementById('bouton').innerHTML='Cacher le texte';
     }
     else
     {
         document.getElementById(id).style.visibility="hidden";
-        document.getElementById('bouton_'+id).innerHTML='Coordonnées';
+        document.getElementById('bouton').innerHTML='Afficher le texte';
     }
     return true;
 }
+*/
